71ec8eadb2b50c8f5489bfd0bfa48ce21922778d
Colin Clark <colinbdclark@gmail.com>
2024-11-19 12:32:01 -0500

    Resolves gh-19 and gh-21: Normalizes keypoints to a consistent format.

--- README.md
+++ README.md
@@ -12,7 +12,55 @@ MovementOSC is an Electron-based app that is supported on macOS, Windows, and Li
 
 ## Keypoints
 
-The TensorFlow pose detection documentation provides images that show the [correspondence between keypoints and bodies](https://github.com/tensorflow/tfjs-models/tree/cd45df42c8a7605da649789aefb5ae3745f32592/pose-detection#keypoint-diagram).
+MovementOSC provides a normalized keypoint format that is consistent across all its supported models:
+
+* 33 keypoints in total
+* the first 16 keypoints are the COCO set
+* the remaining 17 keypoints are those unique to BlazePose, in order from head to toe
+* regardless of which model you use, all 33 keypoints will be provided; keypoints that aren't supported by the model, or which don't meet the specified minimum score, will be set to NaN
+
+### List of Keypoints
+
+```
+0: nose
+1: left_eye
+2: right_eye
+3: left_ear
+4: right_ear
+5: left_shoulder
+6: right_shoulder
+7: left_elbow
+8: right_elbow
+9: left_wrist
+10: right_wrist
+11: left_hip
+12: right_hip
+13: left_knee
+14: right_knee
+15: left_ankle
+16: right_ankle
+17: left_eye_inner
+18: left_eye_outer
+19: right_eye_inner
+20: right_eye_outer
+21: mouth_left
+22: mouth_right
+23: left_pinky
+24: right_pinky
+25: left_index
+26: right_index
+27: left_thumb
+28: right_thumb
+29: left_heel
+30: right_heel
+31: left_foot_index
+32: right_foot_index
+```
+
+### Keypoint Diagram
+
+![Image showing the location of each keypoint on a stick figure](docs/images/keypoints_stick_figure.png)
+
 
 # Credits
 MovementOSC is Copyright 2024 [Kinetic Light](https://kineticlight.org) and [Lichen Community Systems Worker Cooperative Canada](https://lichen.coop), and was created by [Colin Clark](https://colinclark.org).
--- src/renderer/pose-transformer.js
+++ src/renderer/pose-transformer.js
@@ -1,4 +1,119 @@
-const NOT_RECOGNIZED = NaN;
+const KEYPOINT_NOT_RECOGNIZED = NaN;
+const NUM_NORMALIZED_KEYPOINTS = 33;
+
+const KEYPOINT_FORMAT = {
+    COCO: 0,
+    BLAZEPOSE: 1,
+    UNKNOWN: -1
+};
+
+const KEYPOINT_NAMES = [
+    "nose",
+    "left_eye",
+    "right_eye",
+    "left_ear",
+    "right_ear",
+    "left_shoulder",
+    "right_shoulder",
+    "left_elbow",
+    "right_elbow",
+    "left_wrist",
+    "right_wrist",
+    "left_hip",
+    "right_hip",
+    "left_knee",
+    "right_knee",
+    "left_ankle",
+    "right_ankle",
+    "left_eye_inner",
+    "left_eye_outer",
+    "right_eye_inner",
+    "right_eye_outer",
+    "mouth_left",
+    "mouth_right",
+    "left_pinky",
+    "right_pinky",
+    "left_index",
+    "right_index",
+    "left_thumb",
+    "right_thumb",
+    "left_heel",
+    "right_heel",
+    "left_foot_index",
+    "right_foot_index"
+];
+
+const NORMALIZED_TO_BLAZEPOSE = [
+    0, 2, 5, 7, 8, 11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28,
+    1, 3, 4, 6, 9, 10, 17, 18, 19, 20, 21, 22, 29, 30, 31, 32
+];
+
+function detectKeypointFormat(poses) {
+    // TODO: Provide explicit metadata about how to map
+    // keypoints for each model.
+    if (poses.length < 1) {
+        return KEYPOINT_FORMAT.UNKNOWN;
+    }
+
+    switch (poses[0].keypoints.length) {
+        case 17:
+            return KEYPOINT_FORMAT.COCO;
+        case 33:
+            return KEYPOINT_FORMAT.BLAZEPOSE;
+        default:
+            return KEYPOINT_FORMAT.UNKNOWN;
+    }
+}
+
+function expandCOCOKeypoints(keypoints) {
+    for (let i = 17; i < NUM_NORMALIZED_KEYPOINTS; i++) {
+        keypoints[i] = {
+            name: KEYPOINT_NAMES[i],
+            score: 0.0,
+            x: KEYPOINT_NOT_RECOGNIZED,
+            y: KEYPOINT_NOT_RECOGNIZED,
+            z: KEYPOINT_NOT_RECOGNIZED
+        }
+    }
+
+    return keypoints;
+}
+
+function rearrangeBlazePoseKeypoints(keypoints) {
+    let rearranged = [];
+
+    for (let i = 0; i < NUM_NORMALIZED_KEYPOINTS; i++) {
+        let blazePoseIdx = NORMALIZED_TO_BLAZEPOSE[i];
+        rearranged.push(keypoints[blazePoseIdx]);
+    }
+
+    return rearranged;
+}
+
+function normalizePoseKeypoints(pose, format) {
+    let keypoints = pose.keypoints;
+
+    if (format === KEYPOINT_FORMAT.COCO) {
+        pose.keypoints = expandCOCOKeypoints(keypoints);
+    } else if (format === KEYPOINT_FORMAT.BLAZEPOSE) {
+        pose.keypoints = rearrangeBlazePoseKeypoints(keypoints);
+    }
+}
+
+function filterKeypoint(keypoint, minimumScore) {
+    if (keypoint.score < minimumScore) {
+        keypoint.x = KEYPOINT_NOT_RECOGNIZED;
+        keypoint.y = KEYPOINT_NOT_RECOGNIZED;
+        keypoint.z = KEYPOINT_NOT_RECOGNIZED;
+    }
+}
+
+function normalizeKeypointValues(keypoint, w, h) {
+    keypoint.x = keypoint.x / w;
+    keypoint.y = keypoint.y / h;
+    keypoint.z = keypoint.z !== undefined ?
+        (keypoint.z / w) : KEYPOINT_NOT_RECOGNIZED;
+}
 
 export class PoseTransformer {
     constructor(video) {
@@ -12,21 +127,15 @@ export class PoseTransformer {
     }
 
     transform(poses, minimumScore) {
-        // TODO: Reorder keypoints.
+        let format = detectKeypointFormat(poses);
         for (let i = 0; i < poses.length; i++) {
             let pose = poses[i];
+            normalizePoseKeypoints(pose, format);
+
             for (let j = 0; j < pose.keypoints.length; j++) {
                 let keypoint = pose.keypoints[j];
-                if (keypoint.score < minimumScore) {
-                    keypoint.x = NOT_RECOGNIZED;
-                    keypoint.y = NOT_RECOGNIZED;
-                    keypoint.z = NOT_RECOGNIZED;
-                } else {
-                    keypoint.x = keypoint.x / this.w;
-                    keypoint.y = keypoint.y / this.h;
-                    keypoint.z = keypoint.z !== undefined ?
-                        (keypoint.z / this.w) : NOT_RECOGNIZED;
-                }
+                normalizeKeypointValues(keypoint, this.w, this.h);
+                filterKeypoint(keypoint, minimumScore);
             }
         }
 
